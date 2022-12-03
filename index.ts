1e07b09159f17d71e8652b08abe04283c73426e0
Ian Harris <git@ianharris.io>
2022-12-03 13:03:06 -0700

    Upgrade eslint and related packages

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { CLIEngine, Linter } from 'eslint';
+import { Linter } from "eslint";
 
-type Config = CLIEngine.Options & Linter.Config;
+type Config = Linter.Config;
 
 const eslintConfig: Config = {
   parser: "@typescript-eslint/parser",
@@ -12,11 +12,12 @@ const eslintConfig: Config = {
   },
   extends: [
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
   ],
   parserOptions: {
     ecmaVersion: 2019,
-    sourceType: "module"
+    sourceType: "module",
   },
   rules: {
     "prettier/prettier": "warn",
@@ -25,8 +26,8 @@ const eslintConfig: Config = {
     "@typescript-eslint/no-explicit-any": 0,
     "@typescript-eslint/camelcase": 0,
     "@typescript-eslint/no-use-before-define": 0,
-    "@typescript-eslint/no-inferrable-types": 0
-  }
+    "@typescript-eslint/no-inferrable-types": 0,
+  },
 };
 
 export = eslintConfig;
